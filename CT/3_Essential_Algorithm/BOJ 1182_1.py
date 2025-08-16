dcc27e0b83925d5fcc4eab308f8fdcdbf4c5d9b4
gaptop <dkdlwkrsbxjs@naver.com>
2025-08-16 20:05:53 +0900

    2025.08.16_ct

---
+++ CT/3_Essential_Algorithm/BOJ 1182_1.py
@@ -0,0 +1,28 @@
+def search(lev):
+    global N, S, arr, cur_sum, ans
+
+    # base case
+    if lev == N:
+        if cur_sum == S:
+            ans += 1
+        return
+
+    # 인덱스가 lev인 원소 선택 O
+    cur_sum += arr[lev]
+    search(lev + 1)
+    cur_sum -= arr[lev]
+
+    # 인덱스가 lev인 원소 선택 X
+    search(lev + 1)
+
+
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+cur_sum = 0
+ans = 0
+
+search(0)
+if S == 0:
+    ans -= 1
+
+print(ans)
